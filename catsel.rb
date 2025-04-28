a911725c6cc4bb4b7fb782f6290613bbe1cb9b02
Alex Arroyo <alexaldearroyo@gmail.com>
2025-04-28 17:49:07 +0200

    Update catsel.rb

--- catsel.rb
+++ catsel.rb
@@ -1,8 +1,8 @@
 class Catsel < Formula
   desc "Interactive file selector for concatenating and exporting text files"
   homepage "https://github.com/alexaldearroyo/catselector"
-  url "https://github.com/alexaldearroyo/catselector/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "579dfec00e4395b8d7160ef53bd87a109d4b926089cfba666965708ff4234c87"
+  url "https://github.com/alexaldearroyo/catselector/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "127cbb3a6385707e0ee6a6ac00477667651715720be1d98b94bc2444ca7a1de9"
   license "MIT"
 
   depends_on "go" => :build
